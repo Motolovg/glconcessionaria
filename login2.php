a2bdd3f29d80859b44b544b5b22d2050139ee11d
motolovg <giovanediasmiranda@gmail.com>
2021-04-17 08:24:36 -0300

    criando login bonito

---
+++ login2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+    <div class="center">
+      <input type="checkbox" id="show">
+      <label for="show" class="show-btn">LG concessionaria</label>
+      <div class="container">
+        <label for="show" class="close-btn fas fa-times" title="close"></label>
+        <div class="text">
+Gerente, é você?</div>
+          <form action="realiza_login.php" method="POST">
+                        <div class="data">
+                          <label for="login">Usuário</label>
+                          <input type="text" name="login" id="login" required>
+                        </div>
+              <div class="data">
+                          <label for="senha">Senha</label>
+                          <input type="password" name="senha" id="senha" required>
+                        </div>
+              <div class="forgot-pass">
+              <a href="#">Esqueceu a senha?</a></div>
+              <div class="btn">
+                          <div class="inner">
+              </div>
+              <button type="submit" name="btnLogin" id= "btnlogin">logar</button>
+          </form>          
+</div>
+</body>
+</html>
